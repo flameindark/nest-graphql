9a34aef00159a29b9f6df64c2fc0bd3df838ae96
Liusheng Wang <wangliusheng563@gmail.com>
2024-10-27 23:28:50 +0800

    add: 部分docker配置

---
+++ .config/database.ts
@@ -0,0 +1,11 @@
+const dbConfig = {
+    host: '1.92.84.229',
+    port: 3306,
+    username: 'dbadmin',
+    password: 'Dbadmin@594563',
+    database: 'my_new_database',
+}
+
+export {
+    dbConfig
+}
